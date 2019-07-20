9760ec3f4d9b5b708030c5defd99bb7b9f070062
bradley-corwin <brad.corwin@gmail.com>
2019-07-20 16:12:01 -0500

    completed project 2

--- p2-continuous-control/model.py
+++ p2-continuous-control/model.py
@@ -12,7 +12,7 @@ def hidden_init(layer):
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=400, fc2_units=300):
+    def __init__(self, state_size, action_size, seed, fc1_units=128, fc2_units=128):
         """Initialize parameters and build model.
         Params
         ======
@@ -27,6 +27,7 @@ class Actor(nn.Module):
         self.fc1 = nn.Linear(state_size, fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, action_size)
+        self.bn1 = nn.BatchNorm1d(fc1_units)
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -36,15 +37,18 @@ class Actor(nn.Module):
 
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
+        if state.dim() == 1:
+            state = torch.unsqueeze(state,0)
+        #x = F.relu(self.fc1(state))
+        x = F.leaky_relu(self.bn1(self.fc1(state)), negative_slope=0.01)
+        x = F.leaky_relu(self.fc2(x), negative_slope=0.01)
         return F.tanh(self.fc3(x))
 
 
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, seed, fcs1_units=400, fc2_units=300):
+    def __init__(self, state_size, action_size, seed, fcs1_units=128, fc2_units=128):
         """Initialize parameters and build model.
         Params
         ======
@@ -59,6 +63,7 @@ class Critic(nn.Module):
         self.fcs1 = nn.Linear(state_size, fcs1_units)
         self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
         self.fc3 = nn.Linear(fc2_units, 1)
+        self.bn1 = nn.BatchNorm1d(fcs1_units)
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -68,7 +73,10 @@ class Critic(nn.Module):
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
-        xs = F.relu(self.fcs1(state))
+        #xs = F.relu(self.fcs1(state))
+        if state.dim() == 1:
+            state = torch.unsqueeze(state,0)
+        xs = F.leaky_relu(self.bn1(self.fcs1(state)), negative_slope=0.01)
         x = torch.cat((xs, action), dim=1)
-        x = F.relu(self.fc2(x))
+        x = F.leaky_relu(self.fc2(x), negative_slope=0.01)
         return self.fc3(x)
